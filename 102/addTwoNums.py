d172fe0a0621c6636d472d7bd616ebf0162abac0
Kenny Wang <Kenny.ke.wang@gmail.com>
2018-09-22 22:10:55 +1000

    102

--- 102/addTwoNums.py
+++ 102/addTwoNums.py
@@ -8,29 +8,57 @@
 # Output: 7 -> 0 -> 8
 # Explanation: 342 + 465 = 807.
 
-import node
+from node import ListNode
 
 def addTwoNums(l1, l2):
     l1Num = l2Num = ''
     n1, n2 = l1, l2
-    while(True):
-        l1Num += n1.val
-        if l1.next == None:
+    while True:
+        if n1 is not None:
+            l1Num += str(n1.val)
+
+        if n1.next is None:
             break
         else:
             n1 = n1.next
 
 
-    while(True):
-        l2Num += n2.val
-        if l2.next == None:
+    while True:
+        if n2 is not None:
+            l2Num += str(n2.val)
+
+        if n2.next is None:
             break
         else:
             n2 = n2.next
     
-    sumNum = int(l1Num) + int(l2Num)
-    for i in str(sumNum):
-        nod = Node(i)
+    sumNum = int(l1Num[::-1]) + int(l2Num[::-1])
+    r1 = str(sumNum)[::-1]
+    r2 = list(map(lambda x: ListNode(x), r1))
+    
+    for i in range(len(r1)-1):
+        r2[i].next = r2[i+1]
 
-n = Node(1)
-print(n.val)
+    node = r2[0]
+    return node
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
+n2 = ListNode(2)
+n4 = ListNode(4)
+n3 = ListNode(3)
+n2.next = n4
+n4.next = n3
+
+l5 = ListNode(5)
+l6 = ListNode(6)
+l4 = ListNode(4)
+l5.next = l6
+l6.next = l4
+
+r = addTwoNums(n2, l5)
+
+while r is not None:
+    print(r.val)
+    r = r.next
